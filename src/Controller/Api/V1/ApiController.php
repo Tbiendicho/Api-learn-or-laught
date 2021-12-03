0c602dc2b0990744d7a8348db0494f45fc70358b
Thomas Biendicho <thomas.biendicho@gmail.com>
2021-12-03 13:41:08 +0100

    bug fix on the api controller

---
+++ src/Controller/Api/V1/ApiController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller\Api\V1;
+
+use App\Repository\QuoteRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api", name="api")
+ */
+class ApiController extends AbstractController
+{
+    /**
+     * @Route("", name="api")
+     */
+    public function browseQuotes(QuoteRepository $quoteRepository): Response
+    {
+        $allQuotes = $quoteRepository->findAll();
+    
+        return $this->json($allQuotes, Response::HTTP_OK, [], ['groups' => 'api_quote_browse']);
+    }
+
+    /**
+     * @Route("/{id}", name="api_quote", methods={"GET"})
+     */
+    public function browseQuotesByCategory(QuoteRepository $quoteRepository, $id): Response
+    {
+        $allQuotesByCategory = $quoteRepository->findAllQuotesByCategory($id);
+    
+        return $this->json($allQuotesByCategory, Response::HTTP_OK, [], ['groups' => 'api_quote_browse']);
+    }
+
+        // /**
+    //  * @Route("/api/v1/quote/{id}", name="api_quote", methods={"GET"})
+    //  */
+    // public function browseQuotesBylike(QuoteRepository $quoteRepository, $id): Response
+    // {
+    //     $allQuotesByCategory = $quoteRepository->findAllQuotesByCategory($id);
+    
+    //     return $this->json($allQuotesByCategory, Response::HTTP_OK, [], ['groups' => 'api_quote_browse']);
+    // }
+
+    // public function readRandomQuote(): Response
+    // {
+    //     return $this->render('api/quote/index.html.twig', [
+    //         'controller_name' => 'QuoteController',
+    //     ]);
+    // }
+
+    // public function readRandomQuoteByCategory(): Response
+    // {
+    //     return $this->render('api/quote/index.html.twig', [
+    //         'controller_name' => 'QuoteController',
+    //     ]);
+    // }
+}
